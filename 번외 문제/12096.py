f55c321da27841763867d720a0b8962b8927ada7
DM <happydm09@naver.com>
2023-02-28 16:09:39 +0900

    Submit TEXT

---
+++ 번외 문제/12096.py
@@ -0,0 +1,20 @@
+# 02-521-0487
+'''
+TEXT로 제출했습니다.
+
+해설: 페이지 소스 코드를 보면 주석처리된
+66y47KCc7J2YIOygleuLteydgA==
+7JWM66Ck7KSEIOyImCDsl4bri6Q=
+7ZWY7KeA66eMIO2ejO2KuOuKlCDsnojri6Q=
+7Z6M7Yq464qUIGh0dHBzOi8vc3RhcnRsaW5rLmlvLyDsl5Ag7J6I64qUIOOFiOOFjuOFguOFjg==
+가 있습니다.
+
+이걸 base64로 디코딩하면
+문제의 정답은
+알려줄 수 없다
+하지만 힌트는 있다
+힌트는 https://startlink.io/ 에 있는 ㅈㅎㅂㅎ
+이며 ㅈㅎㅂㅎ는 전화번호를 의미합니다.
+
+따라서 제시된 웹의 전화번호인 02-521-0487이 답입니다.
+'''
