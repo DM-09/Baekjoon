6a9a03d6042345fc7a24d36f8e7dfa3b52182144
DM <happydm09@naver.com>
2023-04-29 16:12:36 +0900

    Create 25206.py

---
+++ 전체 문제/Sliver V/25206.py
@@ -0,0 +1,13 @@
+ts = {'A+': 4.5, 'A0': 4.0, 'B+': 3.5, 'B0': 3.0, 'C+': 2.5,
+      'C0': 2.0, 'D+': 1.5, 'D0': 1.0, 'F': 0.0}
+
+total = 0
+cn = 0
+
+for i in range(20):
+    name, s, t = input().split()
+    if t != 'P':
+        total += float(s) * ts[t]
+        cn += float(s)
+
+print(total / cn)
