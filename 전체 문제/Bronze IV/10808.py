adc1bfcf56c2987e9a6eb4987db5e3b3cb653ceb
DM <happydm09@naver.com>
2023-05-03 16:02:50 +0900

    Create 10808.py

---
+++ 전체 문제/Bronze IV/10808.py
@@ -0,0 +1,7 @@
+s = input()
+al = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in range(len(al)):
+    al[i] = str(s.count(al[i]))
+
+print(' '.join(al))
