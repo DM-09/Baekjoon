561423071fd8ea31a1c44eec8c38f336703b50bb
DM <happydm09@naver.com>
2023-05-10 16:49:24 +0900

    Create 26360.py

---
+++ 전체 문제/Unrated/?Level/26360.py
@@ -0,0 +1,11 @@
+x, n = int(input()), 0
+i = input()
+
+if i == '1': n += x
+if input() == '1':
+    if i == '1':
+        if input() == '1': n += 1
+    else:
+        for c in range(x):
+            if input() == '1': n += 1
+print(n)
