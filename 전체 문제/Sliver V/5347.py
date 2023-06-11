773662afb5ec5a48fe817bb862cce312d3720437
DM <happydm09@naver.com>
2023-06-11 18:46:08 +0900

    Create 5347.py

---
+++ 전체 문제/Sliver V/5347.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+def lcm(a, b):
+    return int(a * b / gcd(a, b))
+
+
+for i in range(int(input())):
+    A, B = map(int, input().split())
+    print(lcm(A, B))
