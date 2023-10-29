23e32f419a4b692818347c74afbe90a0bfa2b7e2
DM <happydm09@naver.com>
2023-10-29 10:08:58 +0900

    Create 30402.py

---
+++ 전체 문제/Bronze IV/30402.py
@@ -0,0 +1,8 @@
+def solve():
+    for _ in range(15):
+        for i in input():
+            if i == "w": print('chunbae'); return
+            elif i == 'b': print('nabi'); return
+            elif i == 'g': print('yeongcheol');return
+            
+solve()
