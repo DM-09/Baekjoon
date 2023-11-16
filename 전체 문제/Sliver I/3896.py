bc11e24de853988f17ad2e54a5e498cdfd18c00a
DM <happydm09@naver.com>
2023-11-16 11:32:13 +0900

    Create 3896.py

---
+++ 전체 문제/Sliver I/3896.py
@@ -0,0 +1,25 @@
+def SOE(MAX, MIN=1):
+    box, l = [], [i + 1 for i in range(MAX)]
+
+    for i in range(MAX):
+        c = l[i]
+        if c != 1 and c != 0:
+            for j in range(2, MAX // c + 1):
+                l[c * j - 1] = 0
+            if c >= MIN: box.append(c)
+    return box
+
+box = SOE(1299709)
+
+for _ in range(int(input())):
+    k = int(input())
+
+    if k in box: print(0)
+    else:
+        pre = end = 0
+
+        for i in box:
+            if i > k: end = i; break
+            pre = i
+
+        print(end - pre)
