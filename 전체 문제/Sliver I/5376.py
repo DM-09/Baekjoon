969959ae9eaf993fb5e6424f903dff11ea255b09
dongmin <happydm09@naver.com>
2023-12-22 16:19:23 +0900

    Create 5376.py

---
+++ 전체 문제/Sliver I/5376.py
@@ -0,0 +1,18 @@
+from fractions import Fraction
+from math import *
+
+def inf(i):
+    integer, decimal = i.split('.')
+    a, b = decimal[:-1].split('(')
+    x, y = 10 ** len(a + b), 10 ** len(a)
+
+    c = int(integer + a + b) - int(integer + a)
+    d = x - y
+
+    GCD = gcd(c, d)
+    return  f'{str(c // GCD)}/{str(d // GCD)}'
+
+for _ in range(int(input())):
+    i = input()
+    if i.count('('): print(inf(i))
+    else: print(Fraction(i))
