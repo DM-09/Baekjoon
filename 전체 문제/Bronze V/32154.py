8cabab883fb49ee8024054149b06dbe758ca8722
dongmin <happydm09@naver.com>
2024-09-16 10:28:30 +0900

    Create 32154.py

---
+++ 전체 문제/Bronze V/32154.py
@@ -0,0 +1,4 @@
+l = {1:"ABCDEFGHJLM", 2:"ACEFGHILM", 3:"ACEFGHILM", 4:"ABCEFGHLM", 5:"ACEFGHLM", 6:"ACEFGHLM", 7:"ACEFGHLM", 8:"ACEFGHLM", 9:"ACEFGHLM", 10:"ABCFGHLM"}
+i = int(input())
+print(len(l[i]))
+print(' '.join(l[i]))
